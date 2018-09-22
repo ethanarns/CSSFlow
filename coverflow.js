e7720a80e0c57f467a2fcad06ce179cb8dbf9664
Ethan Arns <narkro555@gmail.com>
2018-09-22 15:23:50 -0700

    Some cleaning

--- coverflow.js
+++ coverflow.js
@@ -1,25 +1,35 @@
 var paneCount = 4;
 var listIndex = 0;
+var canMove = true;
 function keydown(evt) {
+    if (!canMove) {
+        console.log("Cannot move yet.");
+        return;
+    }
     var dir;
-    if (evt.keyCode === 39) {
+    if (evt.keyCode === 37) {
         dir = "l";
         if (listIndex >= paneCount - 1) {
+            console.log("Out of bounds.");
             return;
         }
         listIndex++;
     }
-    else if (evt.keyCode === 37) {
+    else if (evt.keyCode === 39) {
         dir = "r";
         if (listIndex <= 0) {
+            console.log("Out of bounds");
             return;
         }
         listIndex--;
     }
-    console.log(listIndex);
+    //console.log(listIndex);
     for (var i = 0; i < paneCount; i++) {
+        var diff = Math.abs(listIndex - i);
+        //console.log("" + i + "'s diff: " + diff);
         //console.log("id: pane-" + i);
         var curPane = document.getElementById("pane-" + i);
+        curPane.style.zIndex = 100 - diff;
         if (i === listIndex) {
             curPane.style.animationName = "rotforw" + dir;
         }
@@ -30,5 +40,9 @@ function keydown(evt) {
             curPane.style.animationName = "rotbackl";
         }
     }
+    canMove = false;
+    window.setTimeout(function() {
+        canMove = true;
+    }, 250);
 }
 document.addEventListener("keydown", keydown, false);
