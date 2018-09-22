8772450ca8105bcc9df36429464a2826193fdb64
Ethan Arns <narkro555@gmail.com>
2018-09-22 14:40:45 -0700

    Flow works correctly now

--- coverflow.js
+++ coverflow.js
@@ -1,16 +1,34 @@
 var paneCount = 4;
 var listIndex = 0;
 function keydown(evt) {
+    var dir;
     if (evt.keyCode === 39) {
+        dir = "l";
+        if (listIndex >= paneCount - 1) {
+            return;
+        }
         listIndex++;
     }
     else if (evt.keyCode === 37) {
+        dir = "r";
+        if (listIndex <= 0) {
+            return;
+        }
         listIndex--;
     }
+    console.log(listIndex);
     for (var i = 0; i < paneCount; i++) {
-        console.log("id: pane-" + i);
+        //console.log("id: pane-" + i);
         var curPane = document.getElementById("pane-" + i);
-        console.log(curPane.innerHTML);
+        if (i === listIndex) {
+            curPane.style.animationName = "rotforw" + dir;
+        }
+        else if (i === listIndex - 1) {
+            curPane.style.animationName = "rotbackr";
+        }
+        else if (i === listIndex + 1) {
+            curPane.style.animationName = "rotbackl";
+        }
     }
 }
 document.addEventListener("keydown", keydown, false);
