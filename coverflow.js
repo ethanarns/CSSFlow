4aeff980c9af54a5b966b3d1d1673dcbfe8b6e23
Ethan Arns <narkro555@gmail.com>
2018-09-22 18:02:53 -0700

    Added centering

--- coverflow.js
+++ coverflow.js
@@ -1,25 +1,34 @@
 var paneCount = 6;
 var listIndex = 0;
 var canMove = true;
+var offset = 0;
+var DIST = 50;
+
+function setPanePos(el, pos) {
+    el.style.left = "calc(50% - 30vh + " + pos + "px)";
+}
 
 // Set up existing panes
 function updatePanes(dir) {
     for (var i = 0; i < paneCount; i++) {
         var diff = listIndex - i;
         var curPane = document.getElementById("pane-" + i);
-        
         // Set animation direction
         if (i === listIndex) {
             curPane.style.animationName = "rotforw" + dir;
+            setPanePos(curPane, (i * -DIST) + offset);
         }
         else if (i === listIndex - 1) {
             curPane.style.animationName = "rotbackr";
+            setPanePos(curPane, (i * -DIST) + offset);
         }
         else if (i === listIndex + 1) {
             curPane.style.animationName = "rotbackl";
+            setPanePos(curPane, (i * -DIST) + offset);
         }
         else {
             curPane.style.animationName = (i > listIndex) ? "rotbackl" : "rotbackr";
+            setPanePos(curPane, (i * -DIST) + offset);
         }
         // Set Z-Index
         curPane.style.zIndex = 100 - Math.abs(diff);
@@ -39,6 +48,7 @@ function keydown(evt) {
             return;
         }
         listIndex++;
+        offset += DIST;
     }
     else if (evt.keyCode === 39) {
         dir = "r";
@@ -46,6 +56,7 @@ function keydown(evt) {
             return;
         }
         listIndex--;
+        offset -= DIST;
     }
     updatePanes(dir);
     canMove = false;
