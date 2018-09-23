449cb94a559cc8ec2ff533161c6c795a6d8cd7e6
Ethan Arns <narkro555@gmail.com>
2018-09-23 16:21:23 -0700

    Added template for sequelize

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const path = require('path');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    operatorsAliases: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    storage: './db.sqlite'
+});
 
 const PORT = 3002;
 
+// Access files in these solely by name
 app.use(express.static(path.join(__dirname, "public/css")));
 app.use(express.static(path.join(__dirname, "public/js")));
-app.use(express.static(path.join(__dirname, "public/images")));
+// Prefix with "images/"
+app.use('/images', express.static(path.join(__dirname, "public/images")));
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.listen(PORT);
+app.listen(PORT, function() {
+    console.log("Now listening on port " + PORT);
+});
